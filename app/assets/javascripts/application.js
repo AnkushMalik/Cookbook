9335adcae8913b3dad61b04c849d29010523d293
Ankush Malik <ankushmalik631@gmail.com>
2017-03-31 12:25:58 +0530

    added some styles(I)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,13 +18,7 @@
 //= require cocoon
 //= require_tree .
 
-$('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-    }
-  );
+
   $(document).ready(function(){
   $('.slider').slider({
     full_width: false,
